89a9e9e240520a39e742c631e9b65866a7b43bac
MartinezRosario <rosarioelianamartinez@gmail.com>
2023-06-08 13:42:15 +0200

    creacion de los services

---
+++ app/src/main/java/ar/edu/unju/edm/app/domain/services/userService.java
@@ -0,0 +1,14 @@
+package ar.edu.unju.edm.app.domain.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import ar.edu.unju.edm.app.domain.model.Point;
+import ar.edu.unju.edm.app.domain.model.User;
+
+@Service
+public interface userService {
+    public List <User> getListUsers ();
+    public List <Point> getListPoints ();
+}
