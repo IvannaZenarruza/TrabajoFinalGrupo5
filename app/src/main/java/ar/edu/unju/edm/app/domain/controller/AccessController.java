199f4daf1ca973a1c277726432d614b27b108e1e
IvannaZenarruza <ivannazenarruza24@gmail.com>
2023-06-09 00:10:04 -0300

    Implementación de Controladores

--- app/src/main/java/ar/edu/unju/edm/app/domain/controller/AccessController.java
+++ app/src/main/java/ar/edu/unju/edm/app/domain/controller/AccessController.java
@@ -3,32 +3,37 @@ package ar.edu.unju.edm.app.domain.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import ar.edu.unju.edm.app.domain.model.User;
 
 @Controller
 public class AccessController {
     @Autowired
-    
+    User Turist;
     //Registro en la pagina
-    @GetMapping
+    @GetMapping("/register")
     public ModelAndView access (){
         ModelAndView view= new ModelAndView("register");
+        view.addObject("Turista", Turist);
         return view;
     } 
     //Inicio de sesion en la pagina
-    @GetMapping
+    @GetMapping("/login")
     public ModelAndView login() {  
         ModelAndView view= new ModelAndView("login");
+          view.addObject("Turista", Turist);
         return view;
     }
     @PostMapping
-    public ModelAndView Access(){
+    public ModelAndView Access(@PathVariable("Turista") User Turist){
         ModelAndView view= new ModelAndView("redirect:/login"); 
         return view;
     }
     @PostMapping 
-    public ModelAndView Login(){
+    public ModelAndView Login(@PathVariable("Turista") User Turist){
         ModelAndView view= new ModelAndView("redirect:/login");
         return view;
     }
--- app/src/main/java/ar/edu/unju/edm/app/domain/controller/UserController.java
+++ app/src/main/java/ar/edu/unju/edm/app/domain/controller/UserController.java
@@ -2,30 +2,26 @@ package ar.edu.unju.edm.app.domain.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
-    @GetMapping
-    public ModelAndView add() {
-        return null;
-    }
-
-    @GetMapping
+    @GetMapping("/listPoint")
     public ModelAndView getListPoint() {
         ModelAndView view = new ModelAndView("getListPoint");
         return view;
     }
 
-    @GetMapping
+    @GetMapping("/modifyPoint")
     ModelAndView modifyPoint() {
         ModelAndView view = new ModelAndView("modifyPoint");
         return view;
     }
 
-    @GetMapping
-    ModelAndView deleteTurist() {
-        ModelAndView view = new ModelAndView("deleteTurist");
+    @GetMapping("/delete/{ID}")
+    ModelAndView deleteTurist(@PathVariable("ID") Integer ID) {
+        ModelAndView view = new ModelAndView("redirect:/listPoint");
         return view;
     }
 }
