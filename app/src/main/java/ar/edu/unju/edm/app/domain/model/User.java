380c9668bef72232bf67d79b7be97310f17c8253
Meedrem <jcgmeedrem@gmail.com>
2023-06-23 13:31:37 -0300

    Actualización de modelos

--- app/src/main/java/ar/edu/unju/edm/app/domain/model/User.java
+++ app/src/main/java/ar/edu/unju/edm/app/domain/model/User.java
@@ -1,11 +1,12 @@
 package ar.edu.unju.edm.app.domain.model;
 
+import com.sun.istack.NotNull;
 import org.springframework.stereotype.Component;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Component
 @Entity
@@ -14,50 +15,95 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer ID;
+    @NotNull
+    private String name;
+    @NotNull
+    private String lastName;
+    @NotNull
     private String email;
+    @NotNull
     private String password;
+    @NotNull
     private String nationality;
-    private Boolean state;
+    @NotNull
+    private String type;
+    private Boolean state = true;
 
-    public User(String email, String password, Integer ID, String nationality, Boolean state) {
+    public User() {}
+
+    public User(Integer ID, String name, String lastName, String email, String password, String nationality, String type, Boolean state) {
+        this.ID = ID;
+        this.name = name;
+        this.lastName = lastName;
         this.email = email;
         this.password = password;
-        this.ID = ID;
         this.nationality = nationality;
+        this.type = type;
         this.state = state;
     }
-    public User(){
 
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
     public Integer getID() {
         return ID;
     }
+
     public void setID(Integer ID) {
         this.ID = ID;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getNationality() {
         return nationality;
     }
+
     public void setNationality(String nationality) {
         this.nationality = nationality;
     }
-    public boolean isState() {
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Boolean getState() {
         return state;
     }
+
     public void setState(Boolean state) {
         this.state = state;
     }
-    
+
 }
